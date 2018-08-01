5def22615c0d78c0e2aa9baa7309da2754d02d4e
huzaifah123 <huzaifah.m123@gmail.com>
2018-08-01 17:31:52 +0100

    App currently in the process of development
    -User interface created
    -Firebase authentication
    -Register activity implemented
    -creating login/logout functions

---
+++ SocialNetwork2/app/src/main/java/com/example/huzai/socialnetwork/SetupActivity.java
@@ -0,0 +1,84 @@
+package com.example.huzai.socialnetwork;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SetupActivity extends AppCompatActivity {
+
+    private EditText UserName, FullName, CountryName;
+    private Button SaveInformationbutton;
+    private CircleImageView ProfileImage;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef;
+
+    String currentUserID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID);
+
+        UserName = (EditText) findViewById(R.id.setup_username);
+        FullName = (EditText) findViewById(R.id.setup_fullname);
+        CountryName = (EditText) findViewById(R.id.setup_country);
+        SaveInformationbutton = (Button) findViewById(R.id.setup_information_button);
+        ProfileImage = (CircleImageView) findViewById(R.id.setup_profile_image);
+
+        SaveInformationbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SaveAccountSetupInformation();
+            }
+        });
+
+
+    }
+
+    private void SaveAccountSetupInformation() {
+        String username = UserName.getText().toString();
+        String fullname = FullName.getText().toString();
+        String country = CountryName.getText().toString();
+
+        if(TextUtils.isEmpty(username))
+        {
+            Toast.makeText(this,"Please write your username", Toast.LENGTH_SHORT);
+        }
+        if(TextUtils.isEmpty(fullname))
+        {
+            Toast.makeText(this,"Please write your full name", Toast.LENGTH_SHORT);
+        }
+        if(TextUtils.isEmpty(country))
+        {
+            Toast.makeText(this,"Please write your country", Toast.LENGTH_SHORT);
+        }
+        else
+        {
+            HashMap userMap = new HashMap();
+            userMap.put("username", username);
+            userMap.put("fullname", fullname);
+            userMap.put("country", country);
+            userMap.put("status", username);
+            userMap.put("username", username);
+            userMap.put("username", username);
+
+        }
+    }
+}
